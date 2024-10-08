46733f83c77b92a72c175aea13bffc78f3e049bd
M0ngi <saidanemongi@gmail.com>
2024-10-08 11:04:46 +0100

    init

---
+++ spring-boot/src/main/java/com/mini/projet/gl5/spring_boot/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package com.mini.projet.gl5.spring_boot.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> index() {
+        String[] resp = new String[2];
+        resp[0] = "Hello";
+        resp[1] = "World";
+        return new ResponseEntity<Object>(resp, HttpStatus.OK);
+    }
+
+}
